05bcb2da74262dfa5498bdb4526df3649f3fb6df
pious <109950629+pious2847@users.noreply.github.com>
2024-07-25 14:58:47 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ const config = {
     development: {
       AUTH_EMAIL: "abdulhafis384@gmail.com",
       AUTH_PASS: "hlrr gtdr kkxf bube",
-      DBConnectionLink: "mongodb+srv://abdulhafis2847:YwXKPILeI8AJxWzE@hospitalhub.6tewsgp.mongodb.net/hospitalhub",
+      DBConnectionLink: "mongodb+srv://abdulhafis2847:YwXKPILeI8AJxWzE@hospitalhub.6tewsgp.mongodb.net/",
       PORT: 8080,
       JWT_SECRET: "your_development_secret_key",
     },
     production: {
       AUTH_EMAIL: "abdulhafis384@gmail.com",
       AUTH_PASS: "hlrr gtdr kkxf bube",
-      DBConnectionLink: "mongodb+srv://abdulhafis2847:YwXKPILeI8AJxWzE@hospitalhub.6tewsgp.mongodb.net/hospitalhub",
+      DBConnectionLink: "mongodb+srv://abdulhafis2847:YwXKPILeI8AJxWzE@hospitalhub.6tewsgp.mongodb.net/",
       PORT: process.env.PORT || 8080,
       JWT_SECRET: "your_production_secret_key",
     },
