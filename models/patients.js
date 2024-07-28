82d1b6c7cad54c6c7926f8384a62afa6c095bb01
pious <109950629+pious2847@users.noreply.github.com>
2024-07-28 13:50:48 +0000

    updates

--- models/patients.js
+++ models/patients.js
@@ -1,29 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const PatientSchema = new mongoose.Schema({
-  name: String,
-  dateOfBirth: Date,
-  contactInfo: {
-    phone: String,
-    email: String,
-    address: String
-  },
-  diagnosis: {
-        type: String,
-        required: true,
-      },
-  expenses: [
-    {
+const PatientSchema = new mongoose.Schema(
+  {
+    name: String,
+    dateOfBirth: Date,
+    contactInfo: {
+      phone: String,
+      email: String,
+      address: String,
+    },
+    diagnosis: {
+      type: String,
+      require: true,
+    },
+    expenses: [
+      {
         date: Date,
         description: String,
-        amount: Number
-      }
-  ],
-  status: {
-    type: String,
-    enum: ['Admitted',  'Discharged'],
-    default: 'Admitted'
-  }
-}, {timeStamp: true});
+        amount: Number,
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["Admitted", "Discharged"],
+      default: "Admitted",
+    },
+  },
+  { timeStamp: true }
+);
 
-module.exports = mongoose.model('Patient', PatientSchema);
+module.exports = mongoose.model("Patient", PatientSchema);
